92bfa76ebe611559ff0f6c34318a0adb2fc58a0b
julilll <jleschinskaya@gmail.com>
2023-12-20 23:12:20 +0200

    create the component

---
+++ web/src/components/ShowHidePassword/ShowHidePassword.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+
+import { Label, PasswordField, TextField } from '@redwoodjs/forms'
+
+import Icon from '../Icon/Icon'
+
+const ShowHidePassword = ({ label, name, ...rest }) => {
+  const [isPasswordShowing, setisPasswordShowing] = useState(false)
+
+  const handleClick = () => {
+    setisPasswordShowing((prevValue) => !prevValue)
+  }
+
+  return (
+    <div className="field relative">
+      <Label name={name}>{label}</Label>
+      {isPasswordShowing ? (
+        <TextField name={name} placeholder="" {...rest} />
+      ) : (
+        <PasswordField name={name} placeholder="" {...rest} />
+      )}
+      <button className="absolute right-6 top-8" onClick={handleClick}>
+        <Icon id={isPasswordShowing ? 'eyeOpened' : 'eyeClosed'} size={32} />
+      </button>
+    </div>
+  )
+}
+
+export default ShowHidePassword
