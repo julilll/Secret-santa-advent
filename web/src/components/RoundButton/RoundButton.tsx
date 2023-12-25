6d046094061022c93e4cfac63b71451a8f1cb2d5
julilll <jleschinskaya@gmail.com>
2023-12-26 00:12:27 +0200

    Create RoundButton.tsx

---
+++ web/src/components/RoundButton/RoundButton.tsx
@@ -0,0 +1,18 @@
+import Icon from '../Icon/Icon'
+
+interface RoundButtonProps {
+  status: 'success' | 'warning' | 'error'
+}
+
+const RoundButton = ({ status }: RoundButtonProps) => {
+  console.log(status)
+  return (
+    <button
+      className={`center h-16 w-16 rounded-full border-2 border-white status-${status}`}
+    >
+      <Icon id="plus" />
+    </button>
+  )
+}
+
+export default RoundButton
