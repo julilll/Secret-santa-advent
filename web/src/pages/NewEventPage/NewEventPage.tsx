46716990fb5a7688d5dc03eee3ec8ed5a0447cde
julilll <jleschinskaya@gmail.com>
2024-01-06 19:42:23 +0200

    Create NewEventPage.tsx

---
+++ web/src/pages/NewEventPage/NewEventPage.tsx
@@ -0,0 +1,79 @@
+import { DateField, Form, Label, Submit, TextField } from '@redwoodjs/forms'
+import { navigate, routes } from '@redwoodjs/router'
+import { Metadata, useMutation } from '@redwoodjs/web'
+import { toast } from '@redwoodjs/web/dist/toast'
+
+import Checkbox from 'src/components/Checkbox/Checkbox'
+import HeaderWithRulers from 'src/components/HeaderWithRulers/HeaderWithRulers'
+
+const CREATE_EVENT_MUTATION = gql`
+  mutation createEventMutation(
+    $name: String!
+    $date: DateTime!
+    $sendReminder: Boolean!
+  ) {
+    createEvent(
+      input: { name: $name, date: $date, sendReminder: $sendReminder }
+    ) {
+      createdAt
+      date
+      id
+      name
+    }
+  }
+`
+
+const NewEventPage = () => {
+  const [createEvent, { loading }] = useMutation(CREATE_EVENT_MUTATION, {
+    onCompleted: () => {
+      toast.success('Event was successfully created!')
+      navigate(routes.groupInvite())
+    },
+    onError: (error) => {
+      console.error({ error })
+      toast.error(error.message)
+    },
+  })
+
+  const handleSubmit = (data) => {
+    console.log({ data })
+    createEvent({
+      variables: {
+        name: data.eventName,
+        date: data.eventDate,
+        sendReminder: data.eventReminder,
+      },
+    })
+  }
+
+  return (
+    <>
+      <Metadata title="Set up your event" description="NewEvent page" />
+      <HeaderWithRulers
+        className="mb-6 text-white"
+        heading="Set up your event"
+      />
+      <Form className="auth-form" onSubmit={handleSubmit}>
+        <fieldset disabled={loading}>
+          <div className="field">
+            <Label name="eventName">Event Name</Label>
+            <TextField name="eventName" placeholder="" />
+          </div>
+          <div className="field">
+            <Label name="eventDate">Event Date</Label>
+            <DateField name="eventDate" placeholder="" />
+          </div>
+          <div className="field">
+            <Checkbox
+              name="eventReminder"
+              label="Send out a reminder before event"
+            />
+          </div>
+        </fieldset>
+        <Submit>Submit</Submit>
+      </Form>
+    </>
+  )
+}
+
+export default NewEventPage
