b76ced9428e930041c71f61e6dd8d03bf74d8834
julilll <jleschinskaya@gmail.com>
2024-01-21 21:46:55 +0200

    Update Card.tsx

--- web/src/components/Card/Card.tsx
+++ web/src/components/Card/Card.tsx
@@ -4,15 +4,25 @@ import Icon from '../Icon/Icon'
 export interface cardProps {
   avatar: avatarProps
   name: string
+  handleClose?: (event: React.MouseEvent) => void
   email?: string
   closable?: boolean
 }
 
-const Card = ({ avatar, name, email, closable = true }: cardProps) => {
+const Card = ({
+  avatar,
+  name,
+  handleClose,
+  email,
+  closable = true,
+}: cardProps) => {
   return (
     <div className="relative ml-5 w-[calc(100%-20px)] bg-white py-6 pl-[72px] pr-5">
       {closable && (
-        <button className="absolute right-8 top-8 text-bombay">
+        <button
+          className="absolute right-8 top-8 text-bombay"
+          onClick={(event) => handleClose(event)}
+        >
           <Icon id="close" />
         </button>
       )}
